c0b0688b305afb37e8653a332ede89e617a4c933
mnn <mnn.saag@gmail.com>
2023-11-24 07:38:37 +0300

    u

--- src/BMI-Calculator/app.js
+++ src/BMI-Calculator/app.js
@@ -1,6 +1,13 @@
-const form = document.querySelector('from')
+const form = document.querySelector('from');
 
-form.addEventListener('click',(e)=>{
-  e.preventDefault()
-  
-})
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = 'Please provide a valid height.';
+  }
+});
