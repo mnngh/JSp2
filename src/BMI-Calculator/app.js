4d6ac44a8df82ba6a04dce1041beea37e5fdcfcf
mnn <mnn.saag@gmail.com>
2023-11-25 08:19:09 +0300

    p3 complete

--- src/BMI-Calculator/app.js
+++ src/BMI-Calculator/app.js
@@ -1,6 +1,8 @@
-const form = document.querySelector('from');
+const form = document.querySelector('form');
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
   e.preventDefault();
 
   const height = parseInt(document.querySelector('#height').value);
@@ -8,6 +10,12 @@ form.addEventListener('submit', function(e){
   const results = document.querySelector('#results');
 
   if (height === '' || height < 0 || isNaN(height)) {
-    results.innerHTML = 'Please provide a valid height.';
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
   }
 });
